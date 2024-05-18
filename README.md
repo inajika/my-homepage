6fffc83971597a5c85eff122bb416be702b9a515
inajika <inajika@gmail.com>
2024-05-18 20:53:36 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # my-homepage
 ホームページ
+
+## 環境変数
+`.env.sample`をコピーして`.env`を同じディレクトリに作成します。
+
+- `TZ`
+  - タイムゾーン
+- `NODE_ENV`
+  - 実行環境
+    - 本番環境→`production`
+    - 開発環境→`development`
+
+## アイコン
+- [heroicons](https://github.com/tailwindlabs/heroicons)
+- [Font Awesome](https://fontawesome.com/)
---
+++ web/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,astro}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  // daisyui設定
+  daisyui: {
+    themes: ["light"],
+    darkTheme: "light",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
+}
+
