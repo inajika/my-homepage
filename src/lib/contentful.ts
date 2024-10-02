d918aa29bdca834eb9d168fbcf15faa5145b61e5
inajika <inajika@gmail.com>
2024-10-03 00:03:45 +0900

    fix: ディレクトリ構成を見直し

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -7,9 +7,10 @@ import { createClient } from 'contentful';
 export const contentfulClient = createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.CONTENTFUL_TOKEN,
-  host: import.meta.env.NODE_ENV === 'production'
-    ? 'cdn.contentful.com'
-    : 'preview.contentful.com',
+  host:
+    import.meta.env.NODE_ENV === 'production'
+      ? 'cdn.contentful.com'
+      : 'preview.contentful.com',
 });
 
 /**
@@ -17,25 +18,25 @@ export const contentfulClient = createClient({
  */
 export interface BlogEntry {
   /** コンテンツID */
-  contentTypeId: 'blog_entries',
+  contentTypeId: 'blog_entries';
   /** コンテンツ */
   fields: {
     /** 記事ID(ユニークな整数) */
-    entry_id: EntryFieldTypes.Integer,
+    entry_id: EntryFieldTypes.Integer;
     /** 記事タイトル(短いテキスト) */
-    entry_title: EntryFieldTypes.Text,
+    entry_title: EntryFieldTypes.Text;
     /** 記事本文(リッチテキスト) */
-    entry_body: EntryFieldTypes.RichText,
+    entry_body: EntryFieldTypes.RichText;
     /** アイキャッチ画像 */
-    eyecatch: EntryFieldTypes.AssetLink,
+    eyecatch: EntryFieldTypes.AssetLink;
     /** 記事の概要(短いテキスト) */
-    description: EntryFieldTypes.Text,
-  },
-};
+    description: EntryFieldTypes.Text;
+  };
+}
 
 /**
  * 画像レンダリング用オプション
- * 
+ *
  * 画像の代替テキストは次のように決まります。
  * 最優先→画像の説明
  * 次点→画像のタイトル
@@ -43,7 +44,7 @@ export interface BlogEntry {
  */
 export const renderImageOption = {
   renderNode: {
-    "embedded-asset-block": (node: any) => 
+    'embedded-asset-block': (node: any) =>
       `<img src="${node.data.target.fields.file.url}" alt="${
         node.data.target.fields.description
           ? node.data.target.fields.description
